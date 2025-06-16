d4533fae1c70b6b9b45c820db5b8784a59368090
LFCMerino <143030295+LFCMerino@users.noreply.github.com>
2025-06-15 22:19:22 -0300

    Add files via upload

---
+++ docs/script.js
@@ -0,0 +1,17 @@
+function mostrarNav() {
+    const navMenu = document.querySelector('.nav');
+
+    navMenu.classList.toggle('active');
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    const navLinks = document.querySelectorAll('.nav a');
+    navLinks.forEach(link => {
+        link.addEventListener('click', function() {
+            const navMenu = document.querySelector('.nav');
+            if (navMenu.classList.contains('active')) {
+                navMenu.classList.remove('active');
+            };
+        });
+    });
+});
